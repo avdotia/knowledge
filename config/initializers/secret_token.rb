2235393c403835af8732d528760d468930212e95
Laura <lauraperezfelipe@gmail.com>
2012-08-08 21:17:02 +0100

    Initial commit, I have setup rspec, guard+spork

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Knowledge::Application.config.secret_token = 'd819606fc6bb8885b92cfe7b6532b787c26bde7351c688c023ca12a7cec98fe50de042599de4963a2185979d9c6762cfa11222fb9d47e7b74dafe31005419c7c'
