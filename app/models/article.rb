728835c30de2906867e4621c36c2f5faf86a5331
Laura <lauraperezfelipe@gmail.com>
2012-08-09 14:16:15 +0100

    added title and content tests

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,5 @@
 class Article < ActiveRecord::Base
   attr_accessible :content, :title
+  validates :title, :content, presence: true
+  validates :title, length: { maximum: 50 }
 end
--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -3,8 +3,25 @@ require 'spec_helper'
 describe Article do
   
   before { @article = Article.new(title: "titulo de ejemplo", content: "contenido de ejemplo") }
+#@article is the default subject of the test
+#so we can leave @article out of the test yielding 
   subject { @article }
   
   it { should respond_to(:title) }
   it { should respond_to(:content) }
+#Validate the presence  of attributes' tests
+  it { should be_valid }
+  describe "when the title is not present" do
+    before { @article.title = " " }
+    it { should_not be_valid }
+  end  
+  describe "when the content is not present" do
+    before { @article.content = " " }
+    it { should_not be_valid }
+  end    
+#Lenght validation
+  describe "when the title is too long" do
+    before { @article.title = "." * 51 }
+    it { should_not be_valid }
+  end
 end
