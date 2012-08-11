054b11a4c10dae8c3db6cd2b2a73bc40d40c9b0d
Laura <lauraperezfelipe@gmail.com>
2012-08-11 18:54:37 +0100

    simple_form_for

--- Gemfile
+++ Gemfile
@@ -2,7 +2,7 @@ source 'https://rubygems.org'
 
 gem 'rails', '3.2.7'
 gem 'jquery-rails', '2.0.2'
-
+gem 'simple_form'
 
 # Bundle edge Rails instead:
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,6 +3,7 @@ class ArticlesController < ApplicationController
     @articles = Article.all
   end
   def new
+    @article = Article.new
   end
   def show
     id = params[:id]
@@ -13,10 +14,14 @@ class ArticlesController < ApplicationController
   end  
   def update
     @article = Article.find params[:id]
-    @article.update_attributes!(params[:article])
-    
-    ##POR QUE NO ME FUNCIONA EL FLASH?
+    @article.update_attributes!(params[:article])    
     flash[:notice] = "#{@article.title} was successfully updated."
     redirect_to article_path(@article)
   end  
+  def create
+    @article = Article.create!(params[:article])
+    
+    flash[:notice] = "#{@article.title} was successfully created."
+    redirect_to article_path(@article)
+  end
 end
