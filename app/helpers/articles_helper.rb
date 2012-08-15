55bbff4b743d5aefced1363e88a86f3e78a8273d
Laura <lauraperezfelipe@gmail.com>
2012-08-15 11:32:26 +0100

    added arrows for the table

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -5,7 +5,15 @@ module ArticlesHelper
     else
       direction = 'DESC'
     end
-    displayed_name = "#{image_tag 'dfasfs' } #{name}"
+    n = 0
+    if n == 0
+      displayed_name = "#{image_tag 'arriba.jpg' } #{name}"
+      n = 1
+    else
+      displayed_name = "#{image_tag 'abajo.jpg' } #{name}"
+      n = 0
+    end
+ #   displayed_name = "#{image_tag 'arriba.jpg' } #{name}"
     link_to displayed_name.html_safe, articles_path(order_by: field, direction: direction) 
   end
 end
